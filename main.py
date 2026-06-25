3547ff0f45022b1f50480beb6f30da6db7b544aa
Yesid05 <caicedopacho@hotmail.com>
2026-06-25 17:22:04 -0500

    Trabajando con el inidicador MACD

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from binance.client import Client
 from binance import ThreadedWebsocketManager
 from ta.momentum import RSIIndicator
+from ta.trend import MACD
 from telegram import Bot
 import requests
 from decouple import config
@@ -60,23 +61,37 @@ def calcular_indicadores(df):
     df['RSI10'] = rsi.rsi()
     
     df['VOL_MA20'] = df['volume'].rolling(20).mean()
+    
+    macd = MACD(
+        close=df['close'],
+        window_slow=26,
+        window_fast=12,
+        window_sign=9
+    )
+    
+    df['macd'] = macd.macd()
+    df['signal'] = macd.macd_signal()
+    df['histogram'] = macd.macd_diff()
+    
     return df
+
 def detectar_tendencia(df):
     
     ultimo = df.iloc[-1]
     ma9 = ultimo['MA9']
     ma48 = ultimo['MA48']
     rsi = ultimo['RSI10']
+    histogram = ultimo['histogram']
     
     #and float(ultimo['volume']) > float(ultimo['VOL_MA20'])
     #and float(ultimo['volume']) < float(ultimo['VOL_MA20'])
     
     
     if ma9 > ma48 and rsi > 50 :
-        return "ALCISTA", ma9, ma48, rsi
+        return "ALCISTA", ma9, ma48, rsi, histogram
     elif ma9 < ma48 and rsi < 50 :
-        return "BAJISTA", ma9, ma48, rsi
-    return "NEUTRAL", ma9, ma48, rsi
+        return "BAJISTA", ma9, ma48, rsi, histogram
+    return "NEUTRAL", ma9, ma48, rsi, histogram
 
 
 RISK_PER_TRADE = 0.01      # 1%
@@ -104,11 +119,26 @@ def calcular_usdc_comprar(balance_cripto, precio):
 def calcular_precio_equilibrio(precio):
     return precio / (1 - 0.001)**2
 
-def senal_compra(ma9, ma48, rsi):
-    return ma9 > ma48 and rsi > 50 and rsi < 80
+def senal_compra(ma9, ma48, rsi, histogram):
+    
+    """
+    # Compra: MACD cruza por encima de la señal
+df['buy_signal'] = (
+    (df['macd'].shift(1) < df['signal'].shift(1)) &
+    (df['macd'] > df['signal'])
+)
 
-def senal_venta(precio_cierre, precio_compra):
-    return precio_cierre >= (precio_compra + 262)
+# Venta: MACD cruza por debajo de la señal
+df['sell_signal'] = (
+    (df['macd'].shift(1) > df['signal'].shift(1)) &
+    (df['macd'] < df['signal'])
+)
+rsi > 50 and rsi < 80
+    """
+    return ma9 < ma48 and histogram > 0
+
+def senal_venta(precio_cierre, precio_compra, histogram):
+    return precio_cierre >= (precio_compra + 131) and histogram < 0
 
 def precio_actual(df):
     return float(df.iloc[-1]["close"])
@@ -141,7 +171,7 @@ def cantidad_vender(precio_actual):
     
 def stop_loss(precio_actual):
     
-    distancia_al_stop_loss = precio_actual * 0.0015
+    distancia_al_stop_loss = precio_actual * 0.003
     
     return precio_actual - distancia_al_stop_loss
 
@@ -193,15 +223,18 @@ stop_loss_compra = 0
 take_profit_compra = 0
 compra_realizada = False
 venta_realizada = False
+vender_bajo_precio = False
 precio_de_compra = 0
 
 def ejecutar_estrategia():
     try:
-        
+                
         global compra_realizada
         global venta_realizada
+        global vender_bajo_precio
         global precio_de_compra
         global precio_equilibrio
+        global precio_perdida
         global cantidad_comprada
         global crypto
         global usdc
@@ -212,24 +245,30 @@ def ejecutar_estrategia():
         df = obtener_datos(SYMBOL, INTERVAL)
         df = calcular_indicadores(df)
         
-        tendencia, ma9, ma48, rsi = detectar_tendencia(df)
+        print(df)
+        
+        tendencia, ma9, ma48, rsi, histogram = detectar_tendencia(df)
         print(
             f"[{pd.Timestamp.now()}] "
             f"Tendencia: {tendencia} | "
             f"MA9={ma9:.2f} | "
             f"MA48={ma48:.2f} | "
             f"RSI10={rsi:.2f}"
+            f"HISTOGRAM={histogram:.2f}"
         )
         
         if ma9 < ma48 and venta_realizada == True:
             venta_realizada = False
             compra_realizada = False
         
+        if ma9 > ma48 and compra_realizada == True:
+            vender_bajo_precio = True
+        
         precio_ahora = precio_actual(df)
         
-        if tendencia == "ALCISTA" and compra_realizada == False:
+        if tendencia == "BAJISTA" and compra_realizada == False:
             
-            if senal_compra(ma9, ma48, rsi) == True:
+            if senal_compra(ma9, ma48, rsi, histogram) == True:
                 
                 print(f"Precio actual {precio_ahora}")
                 
@@ -256,14 +295,15 @@ def ejecutar_estrategia():
                 )
                 
                 compra_realizada = True
+                vender_bajo_precio = False
         
         else:
             print(f"Compra realizada {compra_realizada}")
-            print(f"Señal de venta {senal_venta(precio_cierre=precio_ahora,precio_compra=precio_de_compra)}")
+            print(f"Señal de venta {senal_venta(precio_cierre=precio_ahora,precio_compra=precio_de_compra,histogram=histogram)}")
             print(f"Vanta realizada {venta_realizada}")
             if compra_realizada == True and venta_realizada == False:
                 
-                if senal_venta(precio_cierre=precio_ahora,precio_compra=precio_de_compra) == True:
+                if senal_venta(precio_cierre=precio_ahora,precio_compra=precio_de_compra, histogram=histogram) == True:
                     
                     cantidad_vendida = cantidad_vender(precio_actual=precio_ahora)
                     usdc = cantidad_vendida
@@ -282,11 +322,30 @@ def ejecutar_estrategia():
                     
                     venta_realizada = True
                     
-                elif ma9 <= ma48:
+                elif ma9 <= ma48 and vender_bajo_precio == True:
                     
                     cantidad_vendida = cantidad_vender(precio_actual=precio_ahora)
                     usdc = cantidad_vendida
                     print(f"Cantidad vender {cantidad_vendida} USDC")
+                    
+                    vender(precio_ahora,cantidad_vendida,stop_loss_compra,take_profit_compra)
+                    
+                    enviar_alerta(
+                        f"[{pd.Timestamp.now()}] \n"
+                        f"===VENTA REALIZADA=== \n "
+                        f"Precio de venta: {precio_ahora} USDC \n "
+                        f"Precio equilibrio {precio_equilibrio} USDC \n "
+                        f"Cantidad vender {cantidad_vendida} USDC \n "
+                        f"Balance {usdc}"
+                    )
+                    
+                    venta_realizada = True
+                
+                elif precio_ahora <= precio_perdida:
+                    
+                    cantidad_vendida = cantidad_vender(precio_actual=precio_ahora)
+                    usdc = cantidad_vendida
+                    print(f"Cantidad vender precio perdida {cantidad_vendida} USDC")
                     
                     vender(precio_ahora,cantidad_vendida,stop_loss_compra,take_profit_compra)
                     
@@ -306,7 +365,7 @@ def ejecutar_estrategia():
                 
     except Exception as e:
         print("Error:", e.with_traceback())
-    
+  
 def manejar_mensaje(msg):
 
     kline = msg['k']
